924cd246c16f8ac563de1e564669a9ba451f3f96
Karl Hasselbring <karl.hasselbring@gmx.de>
2018-07-28 12:14:41 +0200

    Script

--- Script.sh
+++ Script.sh
@@ -2,7 +2,7 @@
 #PBS -N Compression
 #PBS -j oe
 #PBS -m ae
-#PBS -l nodes=4:ppn=4
+#PBS -l nodes=1:ppn=8
 #PBS -l walltime=00:00:05
 #PBS -l mem=128mb
 #PBS -W x=PARTITION:tane
@@ -13,4 +13,5 @@ module load foss
 
 make
 
+mpirun -np 1 ./main
 mpirun ./main
